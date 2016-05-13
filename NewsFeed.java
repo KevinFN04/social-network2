96bfefbf9f3e12264fdeb26b67fc49042bce8eaa
KevinFN04 <kevin.felix.04@gmail.com>
2016-05-13 11:53:47 +0200

    Primer commit

---
+++ NewsFeed.java
@@ -0,0 +1,33 @@
+
+import java.util.ArrayList;
+
+/**
+ * Write a description of class NewsFeed here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class NewsFeed
+{
+    private ArrayList<MessagePost> messages;
+    private ArrayList<PhotoPost> photos;
+
+    /**
+     * Constructor for objects of class NewsFeed
+     */
+    public NewsFeed()
+    {
+        messages = new ArrayList<>();
+        photos = new ArrayList<>();
+    }
+
+    /**
+     * Add a post with a message
+     * 
+     * @message the message to introduce
+     */
+    public void addMessagePost(MessagePost message)
+    {
+        messages.add(message);
+    }
+}
